2945a13db60c3530e441a3ead5a01ae9a11bcc23
kmandt <karsten.mandt@smail.th-koeln.de>
2026-08-07 18:27:19 +0200

    Move RainbowIslandCave script to Map Scripts 20

--- data/maps/scripts.asm
+++ data/maps/scripts.asm
@@ -111,10 +111,10 @@ INCLUDE "maps/Route120.asm"
 INCLUDE "maps/TeamRocketBaseB2FOffice.asm"
 INCLUDE "maps/TeamRocketBaseB1F.asm"
 INCLUDE "maps/TeamRocketBaseB2F.asm"
+INCLUDE "maps/VictoryRoad2F.asm"
 INCLUDE "maps/ForkedStraitWestExit.asm"
 INCLUDE "maps/RainbowIsland.asm"
-INCLUDE "maps/RainbowIslandCave.asm"
-INCLUDE "maps/CrownCityScopeLensHouse.asm"
+
 
 
 
@@ -247,6 +247,7 @@ INCLUDE "maps/StandGym.asm"
 INCLUDE "maps/SunpointPokecenter1F.asm"
 INCLUDE "maps/CrownCitySpeechHouse.asm"
 INCLUDE "maps/SunpointPhotoStudio.asm"
+INCLUDE "maps/CrownCityScopeLensHouse.asm"
 INCLUDE "maps/PokeSeersHouse.asm"
 INCLUDE "maps/BattleTower1F.asm"
 INCLUDE "maps/BattleTowerBattleRoom.asm"
@@ -410,7 +411,8 @@ INCLUDE "maps/SeafoamIslands1F.asm"
 INCLUDE "maps/SeafoamIslandsB1F.asm"
 INCLUDE "maps/KantoMuseum1F.asm"
 INCLUDE "maps/KantoMuseum2F.asm"
-INCLUDE "maps/VictoryRoad2F.asm"
+INCLUDE "maps/RainbowIslandCave.asm"
+
 
 SECTION "Map Scripts 21", ROMX
 
